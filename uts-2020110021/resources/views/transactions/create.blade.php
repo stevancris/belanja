5dca77c7c8eaaabc873798cd5bf0a6d4fabe8bb4
stevancris <cristian.stevanus@reku.id>
2023-10-26 21:59:01 +0700

    add script validation of input select

--- uts-2020110021/resources/views/transactions/create.blade.php
+++ uts-2020110021/resources/views/transactions/create.blade.php
@@ -25,11 +25,18 @@
                     <label for="amount" class="form-label">Amount</label>
                     <input type="text" class="form-control" id="amount" name="amount" value="{{ old('amount') }}">
                 </div>
-                <div class="mb-3 col-md-8 col-sm-12">
-                    <label for="type" class="form-label">Type</label>
-                    <select type="text" class="form-control" name="type" value="{{ old('type') }}">
+                <div class="mb-3 col-md-12 col-sm-12">
+                    <label for="tipe">Type</label>
+                    <select class="form-control" name="tipe" id="tipe" value="{{ old('tipe') }}">
+                        <option value="">Choose</option>
                         <option value="income">Income</option>
                         <option value="expense">Expense</option>
+                    </select>
+                </div>
+                <div class="mb-3 col-md-12 col-sm-12" id="inputCategory">
+                    <label for="category">Category</label>
+                    <select class="form-control" name="category" id="category" value="{{ old('category') }}">
+                        <option value="">Choose</option>
                     </select>
                 </div>
                 <div class="mb-3 col-md-12 col-sm-12">
@@ -38,6 +45,68 @@
                 </div>
                 <button type="submit" class="btn btn-primary btn-block">Save</button>
             </form>
+
+            <script>
+                document.getElementById('tipe').addEventListener('change', function() {
+                    var tipeValue = this.value;
+                    var categorySelect = document.getElementById('category');
+
+                    categorySelect.innerHTML = '<option value="">Choose</option>';
+
+                    if (tipeValue === 'income') {
+                        var option1 = document.createElement('option');
+                        option1.value = 'wage';
+                        option1.text = 'Wage';
+                        categorySelect.appendChild(option1);
+
+                        var option2 = document.createElement('option');
+                        option2.value = 'bonus';
+                        option2.text = 'Bonus';
+                        categorySelect.appendChild(option2);
+
+                        var option3 = document.createElement('option');
+                        option3.value = 'gift';
+                        option3.text = 'Gift';
+                        categorySelect.appendChild(option3);
+                    }
+                    else if (tipeValue === 'expense') {
+                        var option4 = document.createElement('option');
+                        option4.value = 'food_and_drinks';
+                        option4.text = 'Food & Drinks';
+                        categorySelect.appendChild(option4);
+
+                        var option5 = document.createElement('option');
+                        option5.value = 'shopping';
+                        option5.text = 'Shopping';
+                        categorySelect.appendChild(option5);
+
+                        var option6 = document.createElement('option');
+                        option6.value = 'charity';
+                        option6.text = 'Charity';
+                        categorySelect.appendChild(option6);
+
+                        var option7 = document.createElement('option');
+                        option7.value = 'housing';
+                        option7.text = 'Housing';
+                        categorySelect.appendChild(option7);
+
+                        var option8 = document.createElement('option');
+                        option8.value = 'insurance';
+                        option8.text = 'Insurance';
+                        categorySelect.appendChild(option8);
+
+                        var option9 = document.createElement('option');
+                        option9.value = 'taxes';
+                        option9.text = 'Taxes';
+                        categorySelect.appendChild(option9);
+
+                        var option10 = document.createElement('option');
+                        option10.value = 'transportation';
+                        option10.text = 'Transportation';
+                        categorySelect.appendChild(option10);
+                    }
+                });
+            </script>
         </div>
     </div>
 @endsection
